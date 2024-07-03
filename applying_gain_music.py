ea6c89202a38c661470511b6fc00e29fc0518008
Mohammad Naser <mohammad_naser@bose.com>
2024-07-03 17:35:28 -0400

    commit

--- applying_gain_music.py
+++ applying_gain_music.py
@@ -2,67 +2,55 @@ from imports import *
 import user_inputs
 import applying_gain_noise
 import permutation_noise_music
+import music_tf
 
 
 
 
 
 def find_RMS_noise(data):
-    RMS_values = {}
+    RMS_values = []
     for i, recording in enumerate (data.items()):
         my_data = recording[1]
-        RMS_values [i] =  librosa.feature.rms(y=np.array(my_data)).mean()
-    
-    RMS_values_new = {}
-    for key, value in RMS_values.items():
-        RMS_values_new[key] = np.array(value, dtype= np.float32)
-    return RMS_values_new
+        RMS_values.append(librosa.feature.rms(y=np.array(my_data)).mean())
+        RMS_values_avg = np.mean(RMS_values)
+    return RMS_values_avg
 
 def find_RMS_data(data):
-    RMS_values = {}
+    RMS_values = []
     for i, recording in enumerate (data.items()):
         my_data = recording[1]
-        RMS_values [i] =  librosa.feature.rms(y=np.array(my_data)).mean()
-    
-    RMS_values_new = {}
-    for key, value in RMS_values.items():
-        RMS_values_new[key] = np.array(value, dtype= np.float32)
-    return RMS_values_new
+        RMS_values.append(librosa.feature.rms(y=np.array(my_data)).mean())
+        RMS_values_avg = np.mean(RMS_values)
+    return RMS_values_avg
 
 
-def apply_music_gain(noise, audio):
-    noise_rms = find_RMS_noise(noise)
-    noise_rms_arr = np.hstack(list(noise_rms.values()))
-    data_rms = find_RMS_data(audio)
-    data_rms_arr = np.hstack(list(data_rms.values()))
+def apply_music_gain(noise, audio_full, audio_short):
+    noise_rms_avg = find_RMS_noise(noise)
+    data_rms_avg = find_RMS_data(audio_short)
 
-    current_rms_ratio = noise_rms_arr/data_rms_arr
+    current_rms_ratio = noise_rms_avg/data_rms_avg
 
-    length_seg = int(len(current_rms_ratio)/len(user_inputs.sought_ratio))
-    master_c = 0
-    index = 0 
-    new_ratio_dict = {}
-    for i in range(0,len(current_rms_ratio), length_seg):
-        factor=user_inputs.sought_ratio[master_c]
-        chunk = current_rms_ratio[i:i+length_seg]
-        modified_chunk = [factor/x for x in chunk]
-        for value in modified_chunk:
-            new_ratio_dict[index]=value
-            index+=1
-        master_c+=1
+    modified_factors = []
+    for val in user_inputs.sought_ratio:
+        modified_factors.append(current_rms_ratio * val)
+
+    length_seg = int(len(audio_full)/len(user_inputs.sought_ratio))
+    modified_factors_dict = {}
+    i=0
+    beg_index = i
+    for mod_f in modified_factors:
+        end_index = i+length_seg
+        for index in range(beg_index, end_index):
+            modified_factors_dict[i] = mod_f
+            i+=1
+        beg_index=end_index
+
 
     modified_music = {}
-    for key, value in audio.items():
-        modified_music[key] = audio[key]/ new_ratio_dict[key] 
+    for key, value in audio_full.items():
+        modified_music[key] = audio_full[key]/ modified_factors_dict[key] 
     return modified_music
 
 
-
-
-
-
-
-
-
-Data_C = applying_gain_noise.Data_C
-Data_I = apply_music_gain(permutation_noise_music.Data_H, permutation_noise_music.Data_G)
+Data_I = apply_music_gain(applying_gain_noise.Data_C, permutation_noise_music.Data_G,music_tf.Data_F)
